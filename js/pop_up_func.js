bf48963ff0e168837ea5003e5aa474db0b235453
Miguel Montero <miguelmontero@MacBook-Air-de-Miguel.local>
2026-07-29 12:43:17 -0400

    Remove orphaned unused pages (index_video.html, assets/Valparaiso.html); fix homepage popup link to deleted index.html

--- js/pop_up_func.js
+++ js/pop_up_func.js
@@ -1,7 +1,7 @@
  $(window).on('load',function() {
                 new $.popup({                
                     title       : '',
-                    content         : '<a href="index.html#tours"><img src="img/offer.jpg" alt="Image" class="pop_img"><h3 id="pop_msg">Special offer <strong>20% OFF</strong> on selected Tours!</h3></a><small>Autoclose delay in 3 seconds.</small>', 
+                    content         : '<a href="#tours"><img src="img/offer.jpg" alt="Image" class="pop_img"><h3 id="pop_msg">Special offer <strong>20% OFF</strong> on selected Tours!</h3></a><small>Autoclose delay in 3 seconds.</small>',
 					html: true,
 					autoclose   : true,
 					closeOverlay:true,
